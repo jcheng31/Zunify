d2238f885d6812804b9bbc0af6cd08515d5af79d
Jerome Cheng <jcheng31@gmail.com>
2014-01-18 17:07:58 +0800

    Parse album title.

--- Zunify/Models/Track.cs
+++ Zunify/Models/Track.cs
@@ -5,6 +5,9 @@ namespace Zunify.Models
 {
     public class Track
     {
+        public string Title { get; set; }
+        public string AlbumTitle { get; set; }
+
         public static Track FromXElementFactory(XElement element)
         {
             if (element == null)
@@ -12,11 +15,13 @@ namespace Zunify.Models
                 throw new ArgumentException("Received Null in factory method.");
             }
 
-            Track t = new Track {Title = (string) element.Attribute("trackTitle")};
+            Track t = new Track
+            {
+                Title = (string) element.Attribute("trackTitle"),
+                AlbumTitle =  (string) element.Attribute("albumTitle")
+            };
 
             return t;
         }
-
-        public string Title { get; set; }
     }
 }
