a28808d15904ef3e56f80350797a1e5ac26bb484
Jerome Cheng <jcheng31@gmail.com>
2014-01-18 16:51:28 +0800

    Start switching to using XElement instead.

--- Zunify/Models/Track.cs
+++ Zunify/Models/Track.cs
@@ -1,21 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace Zunify.Models
 {
     public class Track
     {
-        public static Track FromXmlStringFactory(string xmlString)
+        public static Track FromXElementFactory(XElement element)
         {
-            if (String.IsNullOrEmpty(xmlString))
-            {
-                throw new ArgumentException();
-            }
-
-            return new Track();
+            throw new NotImplementedException();
         }
+
+        [XmlAttribute("trackTitle")]
+        public string Title { get; set; }
     }
 }
--- ZunifyTests/TrackTests.cs
+++ ZunifyTests/TrackTests.cs
@@ -11,32 +11,11 @@ namespace ZunifyTests
             @"<media src=""D:\Users\Jerome\My Music\Florence + the Machine\Ceremonials (Deluxe Edition)\07 No Light, No Light.mp3"" serviceId=""{6FA7FA06-0100-11DB-89CA-0019B92A3933}"" albumTitle=""Ceremonials (Deluxe Edition)"" albumArtist=""Florence + the Machine"" trackTitle=""No Light, No Light"" trackArtist=""Florence + the Machine"" duration=""274837"" />";
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentException),
-            "Empty String successfully created a Track.")]
-        public void EmptyStringThrowsException()
-        {
-            Track.FromXmlStringFactory(String.Empty);
-        }
-
-        [TestMethod]
         [ExpectedException(typeof (ArgumentException),
-            "Null successfully created a Track.")]
+            "Null created a Track.")]
         public void NullThrowsException()
         {
-            Track.FromXmlStringFactory(null);
-        }
-
-        [TestMethod]
-        public void XmlStringCreatesTrack()
-        {
-            Assert.IsNotNull(Track.FromXmlStringFactory(SampleTrackXml), "Proper XML failed to create a Track.");
-        }
-
-        [TestMethod]
-        public void TitleProperlySet()
-        {
-            Track t = Track.FromXmlStringFactory(SampleTrackXml);
-            Assert.AreEqual("No Light, No Light", t.Title);
+            Track.FromXElementFactory(null);
         }
     }
 }
