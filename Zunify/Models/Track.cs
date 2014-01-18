a7810f0e940f97fde2ada2638652be12b502f202
Jerome Cheng <jcheng31@gmail.com>
2014-01-18 15:47:08 +0800

    Add Track model.

---
+++ Zunify/Models/Track.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zunify.Models
+{
+    public class Track
+    {
+        public static Track FromXmlStringFactory(string xmlString)
+        {
+            if (String.IsNullOrEmpty(xmlString))
+            {
+                throw new ArgumentException();
+            }
+
+            return null;
+        }
+    }
+}
