1d64beae78c6b8716f999816e00e7cea31f03f63
Jerome Cheng <jcheng31@gmail.com>
2014-01-18 17:04:01 +0800

    Add test for artist parsing.

--- ZunifyTests/TrackTests.cs
+++ ZunifyTests/TrackTests.cs
@@ -11,6 +11,14 @@ namespace ZunifyTests
         private const String SampleTrackXml =
             @"<media src=""D:\Users\Jerome\My Music\Florence + the Machine\Ceremonials (Deluxe Edition)\07 No Light, No Light.mp3"" serviceId=""{6FA7FA06-0100-11DB-89CA-0019B92A3933}"" albumTitle=""Ceremonials (Deluxe Edition)"" albumArtist=""Florence + the Machine"" trackTitle=""No Light, No Light"" trackArtist=""Florence + the Machine"" duration=""274837"" />";
 
+        private XElement element;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            element = XElement.Parse(SampleTrackXml);
+        }
+
         [TestMethod]
         [ExpectedException(typeof (ArgumentException),
             "Null created a Track.")]
@@ -22,10 +30,17 @@ namespace ZunifyTests
         [TestMethod]
         public void TrackTitleSetCorrectly()
         {
-            XElement element = XElement.Parse(SampleTrackXml);
             Track t = Track.FromXElementFactory(element);
 
             Assert.AreEqual("No Light, No Light", t.Title, "Track title not set correctly.");
+        }
+
+        [TestMethod]
+        public void TrackArtistSetCorrect()
+        {
+            Track t = Track.FromXElementFactory(element);
+
+            Assert.AreEqual("Ceremonials (Deluxe Edition)", t.AlbumTitle, "Track album not set correctly.");
         }
     }
 }
