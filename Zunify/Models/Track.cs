9ad5101eebdd5b952aa121752bdd0d8f84816f56
Jerome Cheng <jcheng31@gmail.com>
2014-01-18 17:14:17 +0800

    Parse track duration.

--- Zunify/Models/Track.cs
+++ Zunify/Models/Track.cs
@@ -9,6 +9,7 @@ namespace Zunify.Models
         public string AlbumTitle { get; set; }
         public string Artist { get; set; }
         public string AlbumArtist { get; set; }
+        public int Duration { get; set; }
 
         public static Track FromXElementFactory(XElement element)
         {
@@ -22,7 +23,8 @@ namespace Zunify.Models
                 Title = (string) element.Attribute("trackTitle"),
                 AlbumTitle =  (string) element.Attribute("albumTitle"),
                 Artist = (string) element.Attribute("trackArtist"),
-                AlbumArtist = (string) element.Attribute("albumArtist")
+                AlbumArtist = (string) element.Attribute("albumArtist"),
+                Duration = (int) element.Attribute("duration")
             };
 
             return t;
