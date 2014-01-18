6d84fd80f79b39eb4594275bcfa812908ac77cf2
Jerome Cheng <jcheng31@gmail.com>
2014-01-18 15:46:48 +0800

    Add initial Track tests.

---
+++ ZunifyTests/TrackTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Zunify.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZunifyTests
+{
+    [TestClass]
+    public class TrackTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException),
+            "Empty String successfully created a Track.")]
+        public void EmptyStringThrowsException()
+        {
+            Track.FromXmlStringFactory(String.Empty);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentException),
+            "Null successfully created a Track.")]
+        public void NullThrowsException()
+        {
+            Track.FromXmlStringFactory(null);
+        }
+    }
+}
