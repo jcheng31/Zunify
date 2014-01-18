b2344a9aa1e31c7fb5cba82bd658f9e6a34b2aa7
Jerome Cheng <jcheng31@gmail.com>
2014-01-18 17:29:50 +0800

    Add test for null paths.

--- ZunifyTests/PlaylistTests.cs
+++ ZunifyTests/PlaylistTests.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zunify.Models;
 
 namespace ZunifyTests
 {
     [TestClass]
     public class PlaylistTests
     {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException),
+            "Null path didn't throw an exception.")]
+        public void ExceptionOnNullPath()
+        {
+            ZunePlaylist.FromFileFactory(null);
+        }
     }
 }
