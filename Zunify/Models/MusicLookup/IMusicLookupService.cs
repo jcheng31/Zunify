15b0a3db048b37404fcc383a7f20ab7a1526bbca
Jerome Cheng <jcheng31@gmail.com>
2014-01-20 15:32:46 +0800

    Skeleton for SpotifySearchService.

---
+++ Zunify/Models/MusicLookup/IMusicLookupService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zunify.Models.MusicLookup
+{
+    public interface IMusicLookupService
+    {
+        List<Track> FindTrackDetails(String title, String artist, String album);
+    }
+}
---
+++ Zunify/Models/MusicLookup/SpotifySearchService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zunify.Models.MusicLookup
+{
+    public class SpotifySearchService : IMusicLookupService
+    {
+        public List<Track> FindTrackDetails(string title, string artist, string album)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
