3113af3a16f209b495b66a24234db2b6a085a723
Jerome Cheng <jcheng31@gmail.com>
2014-01-20 14:27:54 +0800

    Move implementation from codebehind to MainViewModel.

--- Zunify/MainWindow.xaml.cs
+++ Zunify/MainWindow.xaml.cs
@@ -10,56 +10,9 @@ namespace Zunify
     /// </summary>
     public partial class MainWindow : Window
     {
-        private String playlistPath;
-
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void ChoosePlaylistClick(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog dialog = new OpenFileDialog
-            {
-                Title = "Select Playlist",
-                Filter = "Zune Playlist File (*.zpl)|*.zpl"
-            };
-
-            bool? result = dialog.ShowDialog();
-            if (result != true)
-            {
-                return;
-            }
-
-            playlistPath = dialog.FileName;
-            SelectedPlaylistLabel.Content = playlistPath;
-        }
-
-        private void ParseAndSaveClick(object sender, RoutedEventArgs e)
-        {
-            if (String.IsNullOrWhiteSpace(playlistPath))
-            {
-                return;
-            }
-
-            ZunePlaylist playlist = ZunePlaylist.FromFileFactory(playlistPath);
-
-            SaveFileDialog dialog = new SaveFileDialog
-            {
-                Title = "Save Output",
-                Filter = "Text File (*.txt)|*.txt"
-            };
-
-            bool? result = dialog.ShowDialog();
-            if (result != true)
-            {
-                return;
-            }
-
-            String outputPath = dialog.FileName;
-            String formatString = OutputFormatString.Text;
-            
-            PlaylistConverter.ToTextFile(playlist, outputPath, formatString);
         }
     }
 }
--- Zunify/ViewModel/MainViewModel.cs
+++ Zunify/ViewModel/MainViewModel.cs
@@ -1,4 +1,8 @@
+using System;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Microsoft.Win32;
+using Zunify.Models;
 
 namespace Zunify.ViewModel
 {
@@ -16,9 +20,52 @@ namespace Zunify.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
+        public const String OutputFormatStringPropertyName = "OutputFormatString";
+        public const String PlaylistPathPropertyName = "PlaylistPath";
+
+        private String _playlistPath;
+        private String _outputFormatString;
+
+        public String PlaylistPath
+        {
+            get
+            {
+                return _playlistPath;
+            }
+
+            set
+            {
+                if (_playlistPath == value)
+                {
+                    return;
+                }
+
+                RaisePropertyChanging(PlaylistPathPropertyName);
+                _playlistPath = value;
+                RaisePropertyChanged(PlaylistPathPropertyName);
+            }
+        }
+
+        public String OutputFormatString
+        {
+            get
+            {
+                return _outputFormatString;
+            }
+
+            set
+            {
+                if (_outputFormatString == value)
+                {
+                    return;
+                }
+
+                RaisePropertyChanging(OutputFormatStringPropertyName);
+                _outputFormatString = value;
+                RaisePropertyChanged(OutputFormatStringPropertyName);
+            }
+        }
+        
         public MainViewModel()
         {
             ////if (IsInDesignMode)
@@ -29,6 +76,70 @@ namespace Zunify.ViewModel
             ////{
             ////    // Code runs "for real"
             ////}
+        }
+
+        private RelayCommand _loadPlaylistFileCommand;
+        public RelayCommand LoadPlaylistFileCommand
+        {
+            get
+            {
+                return _loadPlaylistFileCommand
+                    ?? (_loadPlaylistFileCommand = new RelayCommand(ExecuteLoadPlaylistFileCommand));
+            }
+        }
+
+        private void ExecuteLoadPlaylistFileCommand()
+        {
+            OpenFileDialog dialog = new OpenFileDialog
+            {
+                Title = "Select Playlist",
+                Filter = "Zune Playlist File (*.zpl)|*.zpl"
+            };
+
+            bool? result = dialog.ShowDialog();
+            if (result != true)
+            {
+                return;
+            }
+
+            PlaylistPath = dialog.FileName;
+        }
+
+        private RelayCommand _saveParsedTextCommand;
+        public RelayCommand SaveParsedTextCommand
+        {
+            get
+            {
+                return  _saveParsedTextCommand
+                    ?? ( _saveParsedTextCommand = new RelayCommand(ExecuteSaveParsedTextCommand));
+            }
+        }
+
+        private void ExecuteSaveParsedTextCommand()
+        {
+            if (String.IsNullOrWhiteSpace(PlaylistPath))
+            {
+                return;
+            }
+
+            ZunePlaylist playlist = ZunePlaylist.FromFileFactory(PlaylistPath);
+
+            SaveFileDialog dialog = new SaveFileDialog
+            {
+                Title = "Save Output",
+                Filter = "Text File (*.txt)|*.txt"
+            };
+
+            bool? result = dialog.ShowDialog();
+            if (result != true)
+            {
+                return;
+            }
+
+            String outputPath = dialog.FileName;
+            String formatString = OutputFormatString;
+
+            PlaylistConverter.ToTextFile(playlist, outputPath, formatString);
         }
     }
 }
