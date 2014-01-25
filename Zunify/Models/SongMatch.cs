60c83cb41b0dba883c75f07d6a56629163f05794
Jerome Cheng <jcheng31@gmail.com>
2014-01-26 05:26:51 +0800

    Add SongMatch skeleton.

---
+++ Zunify/Models/SongMatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zunify.Models
+{
+    public class SongMatch
+    {
+        public ZuneTrack OriginalTrack { get; private set; }
+        public List<SpotifyTrack> Candidates { get; private set; }
+        public SpotifyTrack MatchedTrack { get; set; }
+
+        public SongMatch(ZuneTrack original, List<SpotifyTrack> candidates)
+        {
+            OriginalTrack = original;
+            Candidates = candidates;
+        }
+
+    }
+}
